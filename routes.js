96f07c44ff22de9feddade3c93ec0ae2a1e2e851
Abhishek Balam <abhishekbalam96@gmail.com>
2018-06-14 14:24:15 +0530

    Thu Jun 14 13:00:27 IST 2018

--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ var db = require('./db.js')
 
 var redis = require('redis')
 // var client = redis.createClient();
-var cleint = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
+var client = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
 
 client.on('connect', function() {
     console.log('Connected to Redis Database');
